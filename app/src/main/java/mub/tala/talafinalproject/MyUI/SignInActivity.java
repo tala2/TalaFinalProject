de0215604a60ed4380fd7d0d526be5cbffb8a538
tala <talamubarki@gmail.com>
2020-11-02 11:43:02 +0200

    My final project 2020

--- app/src/main/java/mub/tala/talafinalproject/MyUI/SignInActivity.java
+++ app/src/main/java/mub/tala/talafinalproject/MyUI/SignInActivity.java
@@ -1,26 +1,100 @@
 package mub.tala.talafinalproject.MyUI;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import mub.tala.talafinalproject.MyUtils.MyValidations;
 import mub.tala.talafinalproject.R;
-
+//1 xml
 public class SignInActivity extends AppCompatActivity {
-    //1
+    //2
     private EditText etEmail,etPassword;
     private Button btnLogin,btnSignup,btnForgetPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-//2
+//3
         etEmail=findViewById(R.id.etEmail2);
         etPassword=findViewById(R.id.etPassWord);
         btnLogin=findViewById(R.id.btnSignIn);
         btnSignup = findViewById(R.id.btnSignup);
         btnForgetPass = findViewById(R.id.btnForgetPass);
+        //4 listner
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //5
+                validateForm();
+            }
+        });
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
     }
+    //5
+    private void validateForm(){
+        //1
+        String email=etEmail.getText().toString();
+        String passw=etPassword.getText().toString();
+        //2
+        boolean isOk=true;
+        //3
+        if (email.length()<5 || email.indexOf('@')==0 || email.indexOf('@')>=email.length()-2 ||
+                email.indexOf('.') == 0 || email.indexOf('.') > email.length() - 1 || email.lastIndexOf('.') < email.indexOf('@')) {
+            isOk = false;
+            etEmail.setError("Wrong Email Address Please Rewrite");
+        }
+        //4
+        MyValidations myValidations = new MyValidations();
+        if (myValidations.validatepassword(passw) == false) {
+            isOk = false;
+            etPassword.setError("Invalid Password!");
+        }
+        //5
+        if (isOk){
+            logIn(email,passw);
+        }
+    }
+    //6
+    private void logIn(String email,String passw)
+    {
+        //7
+        FirebaseAuth auth=FirebaseAuth.getInstance();
+        //8
+        auth.signInWithEmailAndPassword(email,passw).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                //9
+                if(task.isSuccessful())
+                {
+                    //10
+                    Intent i=new Intent(SignInActivity.this,MainActivity.class);
+                    startActivity(i);
+                }
+                //11
+                else
+                {
+                    //12
+                    Toast.makeText(SignInActivity.this,"Faild",Toast.LENGTH_SHORT).show();
+                    etEmail.setError(task.getException().getMessage());
+                }
+            }
+        });
+    }
+
 }
--- app/src/main/java/mub/tala/talafinalproject/MyUI/SignUpActivity.java
+++ app/src/main/java/mub/tala/talafinalproject/MyUI/SignUpActivity.java
@@ -1,11 +1,19 @@
 package mub.tala.talafinalproject.MyUI;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import mub.tala.talafinalproject.MyUtils.MyValidations;
 import mub.tala.talafinalproject.R;
 public class SignUpActivity extends AppCompatActivity {
     private TextView TvSignUp;
@@ -25,13 +33,14 @@ public class SignUpActivity extends AppCompatActivity {
         btnSave = findViewById(R.id.btnSave);//save data
         btnReturn = findViewById(R.id.btnReturn);//return to the home screen
     }
-        private void checkForm() {
+        private void validateForm() {
             String passw2 = etPassWordVarify.getText().toString();
             String passw1 = etPassWord.getText().toString();
             String fname = etFirstName.getText().toString();
             String lname = etLastName.getText().toString();
             String phone = etPhone.getText().toString();
             String email = etEmail2.getText().toString();
+
             boolean isOk = true;
             if (fname.length() < 2)
             {
@@ -44,5 +53,56 @@ public class SignUpActivity extends AppCompatActivity {
                 isOk = false;
                 etEmail2.setError("Wrong Email Address Please Rewrite");
             }
-        }
+            if(passw1.equals(passw2)==false)
+            {
+                isOk=false;
+                etPassWordVarify.setError("passwords must be the same!");
+            }
+            else {
+                MyValidations myValidations = new MyValidations();
+                if (myValidations.validatepassword(passw1) == false) {
+                    isOk = false;
+                    etPassWord.setError("Invalid Password!");
+                }
+            }
+
+            if(isOk)
+            {
+                //toDo: create account and return to sign in screen/close this screen
+                createNewAccount(email,passw1,fname,lname,phone);
+            }
+
+    }
+
+    /**
+     *
+     * @param email
+     * @param passw1
+     * @param fname
+     * @param lname
+     * @param phone
+     */
+    private void createNewAccount(String email, String passw1, String fname, String lname, String phone)
+    {//1
+        FirebaseAuth auth=FirebaseAuth.getInstance();
+        //
+        //2
+        OnCompleteListener<AuthResult> listener=new OnCompleteListener<AuthResult>() {
+            //RESPONS
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(SignUpActivity.this,"Successfuly Signing up",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                else{
+                    Toast.makeText(SignUpActivity.this,"Signing up, Failed"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                    etEmail2.setError("Signing up, Failed"+task.getException().getMessage());
+                }
+
+            }
+        };
+        //3
+        auth.createUserWithEmailAndPassword(email,passw1).addOnCompleteListener(listener);
+    }
 }
