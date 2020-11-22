b5fa34aa093e6db563d105c0bd32baa1628d8101
tala <talamubarki@gmail.com>
2020-11-22 21:38:09 +0200

    My final project 2020

--- app/src/main/java/mub/tala/talafinalproject/data/Customer.java
+++ app/src/main/java/mub/tala/talafinalproject/data/Customer.java
@@ -6,5 +6,38 @@ public class Customer {
     private String phone;
     private String pass;
 
+    public Customer() {
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailAdress() {
+        return EmailAdress;
+    }
+
+    public void setEmailAdress(String emailAdress) {
+        EmailAdress = emailAdress;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
 }
