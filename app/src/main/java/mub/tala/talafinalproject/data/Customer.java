e6eea075eba691f99164da91eacbc9a5e3dfc886
tala <talamubarki@gmail.com>
2020-11-22 21:33:54 +0200

    My final project 2020

--- app/src/main/java/mub/tala/talafinalproject/data/Customer.java
+++ app/src/main/java/mub/tala/talafinalproject/data/Customer.java
@@ -1,4 +1,10 @@
 package mub.tala.talafinalproject.data;
 
 public class Customer {
+   private String name;
+   private String EmailAdress;
+    private String phone;
+    private String pass;
+
+
 }
--- app/src/main/java/mub/tala/talafinalproject/data/Shop.java
+++ app/src/main/java/mub/tala/talafinalproject/data/Shop.java
@@ -1,4 +1,54 @@
 package mub.tala.talafinalproject.data;
 
+import com.google.android.gms.maps.internal.ILocationSourceDelegate;
+
 public class Shop {
+    private String name;
+    private String adress;
+    private String type;
+    private double rate;
+    private ILocationSourceDelegate locationSourceDelegate;
+
+    public Shop() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public ILocationSourceDelegate getLocationSourceDelegate() {
+        return locationSourceDelegate;
+    }
+
+    public void setLocationSourceDelegate(ILocationSourceDelegate locationSourceDelegate) {
+        this.locationSourceDelegate = locationSourceDelegate;
+    }
 }
